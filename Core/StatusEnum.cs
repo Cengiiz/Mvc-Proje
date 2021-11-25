03b562550fd85a2c3ad00faaec032c542d836df9
Cengizhan KOÇAL <cngzkcl53@gmail.com>
2021-11-25 19:36:19 +0300

    Add files via upload

---
+++ Core/StatusEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public static class StatusEnum
+    {
+        public enum Status
+        {
+            Deleted=-1,
+            Passive=0,
+            Active=1
+        }
+    }
+}
